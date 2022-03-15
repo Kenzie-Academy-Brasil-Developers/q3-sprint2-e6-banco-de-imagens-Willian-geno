611ca0a79b5d57cc74a0d9702e95814c282db01a
Willian <willian@pop-os.localdomain>
2022-03-15 14:34:06 -0300

    feat: criado banco de imagens. obs: faltando rota zip

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,68 @@
-# Desenvolva suas rotas aqui
+from http import HTTPStatus
+from unittest import result
+from flask import Flask, jsonify, request, send_file;
+from werkzeug.exceptions import RequestHeaderFieldsTooLarge
+import os
+
+from app.kenzie.image import download_by_name, get_list,liste_by_extencion,upload_image
+
+MAX_CONTENT_LENGTH = os.getenv("MAX_CONTENT_LENGTH")
+
+app = Flask(__name__);
+
+app.config["MAX_CONTENT_LENGTH"] = int(MAX_CONTENT_LENGTH)
+
+@app.errorhandler(413)
+@app.errorhandler(RequestHeaderFieldsTooLarge)
+def app_handle_413(e):
+    return jsonify({"msg":"Arquivo maior que o permitido. Tamanho máximo:1MB"}), HTTPStatus.REQUEST_ENTITY_TOO_LARGE
+
+
+@app.post("/upload")
+def upload():
+    result = ''
+
+    files = request.files.values()
+
+    for file in files:  
+        result = upload_image(file)
+
+    if result == "existe":
+        return jsonify ({'msg':"Já exixte um arquivo com esse nome"}), HTTPStatus.CONFLICT
+    
+    if result == "not extencion":
+        return jsonify({"msg":"Extenção não suportada"}), HTTPStatus.UNSUPPORTED_MEDIA_TYPE
+
+    return jsonify({"msg":"Arquivo criado"}),HTTPStatus.CREATED
+
+
+@app.get("/files/<extension>")
+def list_files_by_extension(extension:str):
+    result = liste_by_extencion(extension)
+
+    if result == "error":
+        return jsonify({"mag":"extenção não exixtente"}), 404 
+
+    return jsonify(result),200
+
+@app.get("/files")
+def list_files():
+    
+    result = get_list()
+
+    return jsonify(result),200
+
+
+@app.get("/download-zip")
+def download_dir_as_zip():
+    ...
+
+
+@app.get("/download/<file_name>")
+def download(file_name:str):
+    result = download_by_name(file_name)
+
+    if result == "error":
+        return jsonify("arquivo não exixtente"), 404 
+
+    return send_file(result, as_attachment=True), 200
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,1 +1,102 @@
-# Desenvolva sua lógica de manipulação das imagens aqui
+import os
+from traceback import print_tb
+from unittest import result
+
+from flask import safe_join
+
+FILES_DIRECTORY = os.getenv("FILES_DIRECTORY")
+ALLOWED_EXTENSIONS = os.getenv("ALLOWED_EXTENSIONS")
+
+
+def get_list():
+    _, dirs,_=next(os.walk("./imagens"))
+    list = []
+    for dir in dirs:
+        *_, files_name = next(os.walk(f'{FILES_DIRECTORY}/{dir}'))
+        list = list + files_name
+
+    return list
+
+
+def liste_by_extencion(extension):
+    list = get_list()
+
+    result = []
+
+    for item in list:
+        if item.split(".")[1] == extension:
+            result = item
+    
+    if result:
+        ...
+    else:    
+        return "error"
+
+    return(result)
+
+
+def download_by_name(name):
+    list = get_list()
+
+    existence = False
+    for item in list: 
+        if item == name:
+            existence = True
+
+    if existence:
+        ...
+    else:
+        return "error"    
+
+
+    result = get_file_path(name)
+
+    return result
+
+
+def get_file_path(filename:str, extencion:str):
+
+    abs_path = os.path.abspath(FILES_DIRECTORY+'/'+extencion)
+    filepath = safe_join(abs_path, filename)
+
+    return filepath
+
+def upload_image(files):
+
+    name = (files.filename)
+    extencion = name.split('.')[1]
+
+    is_extencion = checkin_extencion(extencion)
+    if is_extencion:
+        ...
+    else:
+        return ("not extencion")
+
+    if os.path.exists(f'{FILES_DIRECTORY}/{extencion}'):
+        if os.path.isfile(f'{FILES_DIRECTORY}/{extencion}/{name}'):
+            result = ("existe")
+            return result
+
+        filepath = get_file_path(name, extencion)
+
+        files.save(filepath)
+    else:
+        os.mkdir(f'{FILES_DIRECTORY}/{extencion}')
+        filepath = get_file_path(name, extencion)
+
+        files.save(filepath)
+
+    result = ("criado")
+    return result
+
+def checkin_extencion(extencion):
+    extencions = ALLOWED_EXTENSIONS.split(",")
+
+    is_estencion = False
+
+    for item in extencions:
+        if item == extencion:
+            is_estencion = True
+    
+    return is_estencion
+
